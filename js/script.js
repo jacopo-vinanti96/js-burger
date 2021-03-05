bfb704141e6167c002f53c89216d712598696172
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-05 16:53:50 +0100

    Ritocchi js

--- js/script.js
+++ js/script.js
@@ -3,18 +3,24 @@ var burgerName = document.getElementById('burger-name'),
     addIngredient = document.getElementsByClassName('add-ingredient'),
     opaqueText = document.getElementById('opaque-text'),
     opaqueTextBtn = document.getElementById('opaque-text-2'),
+    opaqueTextEmpty = document.getElementById('opaque-text-3'),
     sum,
     total = document.getElementById('total'),
     calculate = document.getElementById('calculate');
 
 // Se l' utente clicca su "calculate" allora...
 calculate.addEventListener("click", function() {
+  opaqueText.className = "opacity0";
+  opaqueTextBtn.className = "opacity0";
   sum = 50;
   if ( burgerName.value != "" ) {
     for ( var i = 0; i < addIngredient.length; i++ ) {
       if ( addIngredient[i].checked == true ) {
         sum += parseInt(addIngredient[i].value);
       }
+    }
+    if ( sum = 50 ) {
+      opaqueTextEmpty.className = "opacity1";
     }
     total.innerHTML = sum + "€";
   } else {
